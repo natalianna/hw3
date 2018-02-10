83fa531604599539586183ba7661dd0a2f7cad9a
natalianna <35853146+natalianna@users.noreply.github.com>
2018-02-09 16:29:33 -0800

    Create circle-grid.js

---
+++ circle-grid.js
@@ -0,0 +1,12 @@
+
+background(0);
+
+function draw () {
+
+	for (var x = 0; x < width; x = x + 50) {
+  	for (var y = 0; y < height; y = y + 50) {
+  		ellipse(x, y, 25, 25);
+  		fill(255);
+}
+  }
+  }
