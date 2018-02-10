21611e99cc594f4381fddd31b4425c7b0071dcd3
natalianna <35853146+natalianna@users.noreply.github.com>
2018-02-09 22:53:06 -0800

    Create patch.js

---
+++ patch.js
@@ -0,0 +1,21 @@
+
+background(100,0,230);
+
+noFill();
+stroke(255,150,150);
+strokeWeight(10);
+rect(40, 40, 260, 260);
+ellipse(20, 20, 20, 20)
+ellipse(320, 320, 20, 20)
+ellipse(170, 170, 100, 100)
+ellipse(170, 170, 150, 150)
+ellipse(170, 170, 200, 200)
+ellipse(170, 170, 250, 250)
+
+stroke(100,100,255);
+ellipse(170, 170, 225, 225)
+ellipse(170, 170, 175, 175)
+ellipse(170, 170, 125, 125)
+ellipse(170, 170, 75, 75)
+ellipse(320, 20, 20, 20)
+ellipse(20, 320, 20, 20)
